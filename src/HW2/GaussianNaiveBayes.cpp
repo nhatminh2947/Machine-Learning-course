4908787e3138465db3704058043de204ac80c755
Minh Huynh <nhatminh2947@gmail.com>
2019-10-14 21:20:37 +0800

    Adjust smoothing value

--- src/HW2/GaussianNaiveBayes.cpp
+++ src/HW2/GaussianNaiveBayes.cpp
@@ -5,120 +5,104 @@
 #include "GaussianNaiveBayes.h"
 
 void GaussianNaiveBayes::fit(std::vector<Matrix> X, std::vector<int> y) {
-	_train_size = X.size();
-	Matrix mu[10];
-	Matrix sigma[10];
+    _train_size = X.size();
+    Matrix mu[10];
+    Matrix sigma_square[10];
+    Matrix x_square[10];
 
-	for (int id = 0; id < _train_size; ++id) {
-		_prior(y[id], 0)++;
-	}
+    for (int id = 0; id < _train_size; ++id) {
+        _prior(y[id], 0)++;
+    }
 
-	for (int i = 0; i < N_CLASSES; ++i) {
-		mu[i] = sigma[i] = Matrix(28, 28);
-	}
+    for (int i = 0; i < N_CLASSES; ++i) {
+        mu[i] = sigma_square[i] = x_square[i] = Matrix(28, 28);
+    }
 
-	for (int i = 0; i < _train_size; ++i) {
-		mu[y[i]] = mu[y[i]] + X[i];
-	}
+    for (int i = 0; i < _train_size; ++i) {
+        mu[y[i]] = mu[y[i]] + X[i];
+        x_square[y[i]] = x_square[y[i]] + X[i].power(2);
+    }
 
-	for (int i = 0; i < N_CLASSES; ++i) {
-//		std::cout << mu[i] << std::endl;
-		mu[i] = mu[i] / _prior(i, 0);
+    for (int i = 0; i < N_CLASSES; ++i) {
+        mu[i] = mu[i] / _prior(i, 0);
 
-//		std::cout << mu[i] << std::endl;
-		_mean[i] = mu[i].flat();
+        _mean[i] = mu[i].flat();
 
-//		std::cout << _mean[i] << std::endl;
-	}
+        x_square[i] = x_square[i] / _prior(i, 0);
+        x_square[i] = x_square[i].flat();
 
-	for (int i = 0; i < _train_size; ++i) {
-		Matrix A = X[i] - mu[y[i]];
-		sigma[y[i]] = sigma[y[i]] + A.power(2);
-	}
+        for (int row_id = 0; row_id < N_ROWS; ++row_id) {
+            for (int col_id = 0; col_id < N_COLS; ++col_id) {
+                _variance[i](0, row_id * 28 + col_id) = x_square[i](0, row_id * 28 + col_id) -
+                                                        pow(_mean[i](0, row_id * 28 + col_id), 2);
 
-	for (int k = 0; k < N_CLASSES; ++k) {
-		double min_sigma = INT64_MAX;
-		for (int i = 0; i < N_ROWS; ++i) {
-			for (int j = 0; j < N_COLS; ++j) {
-				if (sigma[k](i, j) != 0) {
-					min_sigma = std::min(min_sigma, sigma[k](i, j));
-				}
-			}
-		}
+                if (_variance[i](0, row_id * 28 + col_id) == 0) {
+                    _variance[i](0, row_id * 28 + col_id) = 1500; // SMOOTHING VALUE
+                }
+            }
+        }
+    }
 
-		for (int i = 0; i < N_ROWS; ++i) {
-			for (int j = 0; j < N_COLS; ++j) {
-				if (sigma[k](i, j) == 0) {
-					sigma[k](i, j) = min_sigma;
-				}
-			}
-		}
-	}
-
-	for (int i = 0; i < N_CLASSES; ++i) {
-		sigma[i] = sigma[i] / _train_size;
-		_variance[i] = sigma[i].flat();
-	}
-
-	_prior = _prior / _train_size;
+    _prior = _prior / _train_size;
 }
 
 int GaussianNaiveBayes::predict(Matrix x) {
-	int prediction = -1;
-	double min_prob = INT64_MAX;
+    int prediction = -1;
+    double min_prob = INT64_MAX;
 
-	std::vector<double> log_proba = predict_log_proba(x, true);
+    std::vector<double> log_proba = predict_log_proba(x, true);
 
-	for (int class_id = 0; class_id < N_CLASSES; ++class_id) {
-		std::cout << class_id << ": " << log_proba[class_id] << std::endl;
+    for (int class_id = 0; class_id < N_CLASSES; ++class_id) {
+        std::cout << class_id << ": " << log_proba[class_id] << std::endl;
 
-		if (min_prob > log_proba[class_id]) {
-			min_prob = log_proba[class_id];
-			prediction = class_id;
-		}
-	}
+        if (min_prob > log_proba[class_id]) {
+            min_prob = log_proba[class_id];
+            prediction = class_id;
+        }
+    }
 
-	return prediction;
+    return prediction;
 }
 
 std::vector<double> GaussianNaiveBayes::predict_log_proba(Matrix x, bool norm) {
-	std::vector<double> result;
-	result.reserve(N_CLASSES);
-	x = x.flat();
+    std::vector<double> result;
+    result.reserve(N_CLASSES);
+    x = x.flat();
 
-	for (int i = 0; i < N_CLASSES; ++i) {
-		double probability = 0.0;
-		for (int j = 0; j < 784; ++j) {
+    for (int i = 0; i < N_CLASSES; ++i) {
+        double probability = 0.0;
+        for (int j = 0; j < N_ROWS * N_COLS; ++j) {
 //			double v = (_variance[i](0, j) == 0) ? 100 : _variance[i](0, j);
-			probability += log(1.0 / (sqrt(_variance[i](0, j) * 2 * M_PI))) - (0.5 * pow(x(0, j) - _mean[i](0, j), 2) / _variance[i](0, j));
-		}
-		probability += log(_prior(i, 0));
+            probability += log(1.0 / (sqrt(_variance[i](0, j) * 2 * M_PI))) -
+                           (0.5 * pow((x(0, j) - _mean[i](0, j)), 2) / _variance[i](0, j));
+        }
+        probability += log(_prior(i, 0));
 
-		result.push_back(probability);
-	}
+        result.push_back(probability);
+    }
 
-	if (norm) result = normalize(result);
+    if (norm) result = normalize(result);
 
-	return result;
+    return result;
 }
 
 GaussianNaiveBayes::GaussianNaiveBayes() {
-	for (int i = 0; i < N_CLASSES; ++i) {
-		_mean[i] = _variance[i] = Matrix(1, 28 * 28);
-	}
+    for (int i = 0; i < N_CLASSES; ++i) {
+        _mean[i] = _variance[i] = Matrix(1, 28 * 28);
+    }
 }
 
 void GaussianNaiveBayes::imagination() {
-	for (int class_id = 0; class_id < N_CLASSES; ++class_id) {
-		std::cout << class_id << ":" << std::endl;
-		for (int row_id = 0; row_id < N_ROWS; ++row_id) {
-			for (int col_id = 0; col_id < N_COLS; ++col_id) {
-				int feature_id = 28 * row_id + col_id;
+    for (int class_id = 0; class_id < N_CLASSES; ++class_id) {
+        std::cout << class_id << ":" << std::endl;
+        for (int row_id = 0; row_id < N_ROWS; ++row_id) {
+            for (int col_id = 0; col_id < N_COLS; ++col_id) {
+                int feature_id = 28 * row_id + col_id;
 
-				std::cout << int( _mean[class_id](0, feature_id) >= 128) << " ";
-			}
-			std::cout << std::endl;
-		}
-		std::cout << std::endl;
-	}
+                std::cout << int(_mean[class_id](0, feature_id) >= 128) << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
 }
--- src/HW2/main.cpp
+++ src/HW2/main.cpp
@@ -11,57 +11,57 @@
 #include <memory>
 
 Matrix bin_image(Matrix image) {
-	Matrix result(28, 28);
-	for (int i = 0; i < 28; ++i) {
-		for (int j = 0; j < 28; ++j) {
-			result(i, j) = int(image(i, j)) / 8;
-		}
-	}
+    Matrix result(28, 28);
+    for (int i = 0; i < 28; ++i) {
+        for (int j = 0; j < 28; ++j) {
+            result(i, j) = int(image(i, j)) / 8;
+        }
+    }
 
-	return result;
+    return result;
 }
 
 std::vector<Matrix> ExtractFeatures(const std::vector<Matrix> &images) {
-	std::vector<Matrix> result;
-	result.reserve(images.size());
+    std::vector<Matrix> result;
+    result.reserve(images.size());
 
-	for (auto image : images) {
-		result.push_back(bin_image(image));
-	}
+    for (auto image : images) {
+        result.push_back(bin_image(image));
+    }
 
-	return result;
+    return result;
 }
 
 void hw1(int mode) {
-	std::vector<Matrix> test_images = IdxFileReader::ReadImages("../src/HW2/t10k-images-idx3-ubyte");
-	std::vector<int> test_labels = IdxFileReader::ReadLabels("../src/HW2/t10k-labels-idx1-ubyte");
-	std::vector<Matrix> train_images = IdxFileReader::ReadImages("../src/HW2/train-images-idx3-ubyte");
-	std::vector<int> train_labels = IdxFileReader::ReadLabels("../src/HW2/train-labels-idx1-ubyte");
+    std::vector<Matrix> test_images = IdxFileReader::ReadImages("../src/HW2/t10k-images-idx3-ubyte");
+    std::vector<int> test_labels = IdxFileReader::ReadLabels("../src/HW2/t10k-labels-idx1-ubyte");
+    std::vector<Matrix> train_images = IdxFileReader::ReadImages("../src/HW2/train-images-idx3-ubyte");
+    std::vector<int> train_labels = IdxFileReader::ReadLabels("../src/HW2/train-labels-idx1-ubyte");
 
-	std::unique_ptr<NaiveBayes> classifier;
+    std::unique_ptr<NaiveBayes> classifier;
 
-	if (mode == 0) {
-		train_images = ExtractFeatures(train_images);
-		test_images = ExtractFeatures(test_images);
-		classifier = std::make_unique<MultinomialNaiveBayes>();
-	} else {
-		classifier = std::make_unique<GaussianNaiveBayes>();
-	}
+    if (mode == 0) {
+        train_images = ExtractFeatures(train_images);
+        test_images = ExtractFeatures(test_images);
+        classifier = std::make_unique<MultinomialNaiveBayes>();
+    } else {
+        classifier = std::make_unique<GaussianNaiveBayes>();
+    }
 
-	classifier->fit(train_images, train_labels);
+    classifier->fit(train_images, train_labels);
 
-	int error_rate = 0;
+    int error_rate = 0;
 
-	for (int i = 0; i < 10000; ++i) {
-		std::cout << "Postirior (in log scale):" << std::endl;
-		int test_image_id = i;
-		int prediction = classifier->predict(test_images[test_image_id]);
-		std::cout << "Prediction: " << prediction << ", Ans: " << test_labels[test_image_id] << std::endl << std::endl;
+    for (int test_image_id = 0; test_image_id < 10000; ++test_image_id) {
+        std::cout << "Postirior (in log scale):" << std::endl;
 
-		error_rate += (prediction != test_labels[test_image_id]);
-	}
+        int prediction = classifier->predict(test_images[test_image_id]);
+        std::cout << "Prediction: " << prediction << ", Ans: " << test_labels[test_image_id] << std::endl << std::endl;
 
-	std::cout << "Imagination of numbers in Bayesian classifier:\n" << std::endl;
+        error_rate += (prediction != test_labels[test_image_id]);
+    }
+
+    std::cout << "Imagination of numbers in Bayesian classifier:\n" << std::endl;
 //	for (int class_id = 0; class_id < 10; ++class_id) {
 //		std::cout << class_id << ":" << std::endl;
 //		for (int i = 0; i < 10000; ++i) {
@@ -78,50 +78,50 @@ void hw1(int mode) {
 //		}
 //	}
 
-	classifier->imagination();
+    classifier->imagination();
 
-	std::cout << "Error rate: " << error_rate * 1.0 / 10000 << " (" << error_rate << "/" << "10000)" << std::endl;
+    std::cout << "Error rate: " << error_rate * 1.0 / 10000 << " (" << error_rate << "/" << "10000)" << std::endl;
 }
 
 void hw2(int a, int b) {
-	OnlineLearning onlineLearning(a, b);
-	std::ifstream input("../src/HW2/testfile.txt");
-	std::string event;
-	int cs = 1;
-	while (input >> event) {
-		std::cout << std::setprecision(17) << "Case " << cs << ": " << event << std::endl;
-		int prior_a = onlineLearning.getA();
-		int prior_b = onlineLearning.getB();
-		std::cout << "Likelihood: " << onlineLearning.Event(event) << std::endl;
-		std::cout << "Beta prior: \ta = " << prior_a << "\tb = " << prior_b << std::endl;
-		std::cout << "Beta posterior: a = " << onlineLearning.getA() << "\tb = " << onlineLearning.getB() << std::endl;
-		std::cout << std::endl;
-	}
+    OnlineLearning onlineLearning(a, b);
+    std::ifstream input("../src/HW2/testfile.txt");
+    std::string event;
+    int cs = 1;
+    while (input >> event) {
+        std::cout << std::setprecision(17) << "Case " << cs << ": " << event << std::endl;
+        int prior_a = onlineLearning.getA();
+        int prior_b = onlineLearning.getB();
+        std::cout << "Likelihood: " << onlineLearning.Event(event) << std::endl;
+        std::cout << "Beta prior: \ta = " << prior_a << "\tb = " << prior_b << std::endl;
+        std::cout << "Beta posterior: a = " << onlineLearning.getA() << "\tb = " << onlineLearning.getB() << std::endl;
+        std::cout << std::endl;
+    }
 }
 
 int main(int argc, const char *argv[]) {
-	int hw = 1;
-	int mode = 0;
-	int a = 0;
-	int b = 0;
-	for (int i = 1; i < argc; i++) {
-		std::string para(argv[i]);
-		if (para.find("--hw=") == 0) {
-			hw = std::stoi(para.substr(para.find('=') + 1));
-		} else if (para.find("--mode=") == 0) {
-			mode = std::stoi(para.substr(para.find('=') + 1));
-		} else if (para.find("--a=") == 0) {
-			a = std::stoi(para.substr(para.find('=') + 1));
-		} else if (para.find("--b=") == 0) {
-			b = std::stoi(para.substr(para.find('=') + 1));
-		}
-	}
+    int hw = 1;
+    int mode = 1;
+    int a = 0;
+    int b = 0;
+    for (int i = 1; i < argc; i++) {
+        std::string para(argv[i]);
+        if (para.find("--hw=") == 0) {
+            hw = std::stoi(para.substr(para.find('=') + 1));
+        } else if (para.find("--mode=") == 0) {
+            mode = std::stoi(para.substr(para.find('=') + 1));
+        } else if (para.find("--a=") == 0) {
+            a = std::stoi(para.substr(para.find('=') + 1));
+        } else if (para.find("--b=") == 0) {
+            b = std::stoi(para.substr(para.find('=') + 1));
+        }
+    }
 
-	if (hw == 1) {
-		hw1(mode);
-	} else {
-		hw2(a, b);
-	}
+    if (hw == 1) {
+        hw1(mode);
+    } else {
+        hw2(a, b);
+    }
 
-	return 0;
+    return 0;
 }
