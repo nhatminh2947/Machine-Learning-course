14c078eca94fd8c4a9b41cd20c1dea10cadd6aac
Minh Huynh <nhatminh2947@gmail.com>
2019-11-03 21:10:46 +0800

    add Logistic regression

---
+++ src/HW4/LogisticRegression.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nhatminh2947 on 11/3/19.
+//
+
+#include "LogisticRegression.h"
---
+++ src/HW4/LogisticRegression.h
@@ -0,0 +1,14 @@
+//
+// Created by nhatminh2947 on 11/3/19.
+//
+
+#ifndef HOMEWORK_LOGISTICREGRESSION_H
+#define HOMEWORK_LOGISTICREGRESSION_H
+
+
+class LogisticRegression {
+
+};
+
+
+#endif //HOMEWORK_LOGISTICREGRESSION_H
