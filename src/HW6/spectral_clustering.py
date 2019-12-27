8e9431584ed1575cf09cf1bf5fca37f5ff4f240f
nhatminh2947 <nhatminh2947@gmail.com>
2019-12-27 20:12:58 +0800

    spetral clustering

--- src/HW6/spectral_clustering.py
+++ src/HW6/spectral_clustering.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import kmeans
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -18,10 +19,12 @@ def load_image(file, size=None):
 
 
 class SpectralClustering:
-    def __init__(self, n_clusters=2, normalized=True, max_iter=1000):
+    def __init__(self, n_clusters=2, normalized=True, max_iter=300, gamma_c=0.01, gamma_s=0.01):
         self.normalized = normalized
         self.n_clusters = n_clusters
         self.max_iter = max_iter
+        self.gamma_c = gamma_c
+        self.gamma_s = gamma_s
 
     def compute_rbf_kernel(self, X, X_prime, gamma):
         X_norm = np.matmul(X, X_prime.T)
@@ -30,73 +33,36 @@ class SpectralClustering:
 
         return K
 
-    def gram_matrix(self, X):
-        return X.dot(X.T)
+    def construct_similarity_matrix(self, X, n_rows, n_cols):
+        coor = np.array([[i // n_cols, i % n_cols] for i in range(n_cols * n_rows)])
 
-    def custom_kernel(self, X, gamma_s=None, gamma_c=None):
-        row, col = X.shape[0], X.shape[1]
+        color_similarity = self.compute_rbf_kernel(X, X, self.gamma_c)
+        spatial_similarity = self.compute_rbf_kernel(coor, coor, self.gamma_s)
 
-        X = X.reshape(X.shape[0] * X.shape[1], X.shape[2])
+        return np.multiply(color_similarity, spatial_similarity)
 
-        logging.info(X[0])
+    def fit(self, X):
+        n_rows = n_cols = np.sqrt(X.shape[0])
+        similarity_matrix = self.construct_similarity_matrix(X, n_rows, n_cols)
+        degree_matrix = np.diag(np.sum(similarity_matrix, axis=1))
 
-        if gamma_c is None:
-            gamma_c = 1.0 / X.shape[1]
-
-        if gamma_s is None:
-            gamma_s = 1.0 / X.shape[1]
-
-        rbf_c = self.compute_rbf_kernel(X, X, gamma_c)
-
-        x = np.array([np.full(col, i) for i in range(row)]).reshape(row * col, 1)
-        y = np.array([np.arange(col) for i in range(row)]).reshape(row * col, 1)
-
-        xy = np.hstack([x, y])
-        rbf_s = self.compute_rbf_kernel(xy, xy, gamma_s)
-
-        logging.info(rbf_c[0][0])
-        logging.info(rbf_s)
-
-        return np.multiply(rbf_s, rbf_c)
-
-    def ratio_cut(self, gram_matrix):
-        degree = np.diag(np.sum(gram_matrix, axis=1))
-        L = degree - gram_matrix
-        logging.debug('L shape: {}'.format(L.shape))
-        eigen_values, eigen_vectors = np.linalg.eig(L)
+        laplacian_matrix = similarity_matrix - degree_matrix
+        eigen_values, eigen_vectors = np.linalg.eig(laplacian_matrix)
         idx = np.argsort(eigen_values)[1: self.n_clusters + 1]
         U = eigen_vectors[:, idx].real.astype(np.float32)
 
-        return U
+        classifications = kmeans.KMeans(n_clusters=self.n_clusters).fit(U)
 
-    def clustering(self, data):
-        mu = np.random.randn(self.n_clusters, 2)
-        classification = np.random.random_integers(low=1, high=self.n_clusters, size=data.shape[0])
-
-        iterate = 0
-        while iterate < self.max_iter:
-
-
-            iterate += 1
-
-    def fit(self, data):
-        logging.debug(data.shape)
-
-        weights = self.custom_kernel(data)
-
-        U = self.ratio_cut(weights)
-        logging.debug('U = {}'.format(U))
-
-        logging.info(weights.shape)
-
-        return 0
+        return classifications
 
 
 def main():
     img = load_image('./image1.png', size=(10, 10))
     logging.debug(img)
     spectral_clustering = SpectralClustering(n_clusters=2)
-    spectral_clustering.fit(img)
+    classifications = spectral_clustering.fit(img)
+
+    print('classification: ', classifications)
 
 
 if __name__ == "__main__":
